eaadac7dfc1ad75666bb28cb117976f3250991a9
njamhour <njjamhour@hotmail.com>
2019-09-16 17:08:00 -0300

    Alterado sites

--- resources/views/temp.blade.php
+++ resources/views/temp.blade.php
@@ -15,77 +15,77 @@
 		<tr>
 			<td>JetCoin</td>
 			<td>Multi</td>
-			<td>http://jetcoins.club/r/2590</td>
+			<td><a class="btn btn-primary" href="http://jetcoins.club/r/2590" role="button">Acessar</a></td>
 			<td></td>
 		</tr>
 
                 <tr>
                         <td>Dayuzarce</td>
                         <td>Multi</td>
-                        <td>https://dayuzarce.me/r/568</td>
+                        <td><a class="btn btn-primary" href="https://dayuzarce.me/r/568" role="button">Acessar</a></td>
                         <td></td>
 		</tr>
 
                 <tr>
                         <td>SugarCoin</td>
                         <td>Multi</td>
-                        <td>https://sugarcoin.club/r/971</td>
+                        <td><a class="btn btn-primary" href="https://sugarcoin.club/r/971" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>Free-Coins-Gaby</td>
                         <td>Multi</td>
-                        <td>http://free-coins-gaby.tk/r/332</td>
+                        <td><a class="btn btn-primary" href="http://free-coins-gaby.tk/r/332" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>AutoFaucet(BTC)</td>
                         <td>BTC</td>
-                        <td>https://autofaucet.site?r=1L966gX9GXMt6azs3wqEnC5f33Ufao9Xn5&rc=BTC</td>
+                        <td><a class="btn btn-primary" href="https://autofaucet.site?r=1L966gX9GXMt6azs3wqEnC5f33Ufao9Xn5&rc=BTC" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>AutoFaucet(HORA)</td>
                         <td>Hora</td>
-                        <td>https://hora.freecryptoclaim.site?r=TEZAVHzGbBu1fC3XK2Yy1hQH1gYu9hS1Nk&rc=HORA</td>
+                        <td><a class="btn btn-primary" href="https://hora.freecryptoclaim.site?r=TEZAVHzGbBu1fC3XK2Yy1hQH1gYu9hS1Nk&rc=HORA" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>AutoFaucet(DOGE)</td>
                         <td><Doge/td>
-                        <td>https://doge.autofaucet.site?r=D7BX9UmVm9q8zTJjBScoeZwrBzv7zauZj6&rc=DOGE</td>
+                        <td><a class="btn btn-primary" href="https://doge.autofaucet.site?r=D7BX9UmVm9q8zTJjBScoeZwrBzv7zauZj6&rc=DOGE" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>AutoFaucet(LTC)</td>
                         <td>LTC</td>
-                        <td>https://ltc.autosmdeisy.ltd?r=LRcNgnA9NygbZ4zcHHKhNhjz2HNvxE13aR&rc=LTC</td>
+                        <td><a class="btn btn-primary" href="https://ltc.autosmdeisy.ltd?r=LRcNgnA9NygbZ4zcHHKhNhjz2HNvxE13aR&rc=LTC" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                         <td>BTC Faucet</td>
                         <td>BTC</td>
-                        <td>https://bitfaucet.app/ref/pjmvx</td>
+                        <td><a class="btn btn-primary" href="https://bitfaucet.app/ref/pjmvx" role="button">Acessar</a></td>
                         <td></td>
                 </tr>
 
                 <tr>
                     <td>FreeBTC</td>
                     <td>BTC</td>
-                    <td>https://freebitco.in/?r=4129866</td>
+                    <td><a class="btn btn-primary" href="https://freebitco.in/?r=4129866" role="button">Acessar</a></td>
                     <td></td>
                 </tr>
 
                 <tr>
                     <td>Eobot</td>
                     <td>Multi</td>
-                    <td>https://www.eobot.com/user/2347383</td>
+                    <td><a class="btn btn-primary" href="https://www.eobot.com/user/2347383" role="button">Acessar</a></td>
                     <td></td>
                 </tr>
 
